088f76a194a7bf6c180794bd63909d91d0f6f433
David Case <dacase1@gmail.com>
2026-05-24 09:15:32 -0600

    add annotion to minimization outputs

--- src/afnmr/txmin.c
+++ src/afnmr/txmin.c
@@ -52,14 +52,18 @@ int main( int argc, char *argv[] )
 
    mme_init_sff( prm, frozen, constrained, xyz_ref, NULL );
    iter = -1;   // historical flag to give more verbose output
+   printf("\nEnergy at input geometry:\n");
    energy = mme( xyz, grad, &iter );
+   printf("\nEnergy after applying rattle:\n");
    energy = mme_rattle( xyz, grad, &iter );
 
 //   run the minimization:
 
    char title[] = "minimization";
    energy = xmin( mme_rattle,  &natm, xyz, grad,  &energy,  &grms,  &xo );
+   printf("\nEnergy at final geometry after applying rattle:\n");
    energy = mme_rattle( xyz, grad, &iter );
+   printf("\nEnergy at final geometry without applying rattle:\n");
    energy = mme( xyz, grad, &iter );
    putxv( argv[3], title, natm, start_time, xyz, xyz );
 
